c8acb7386d021ecb16dc88eb942970553d594e0c
Pankaj kumar <pankaj120099@gmail.com>
2026-03-03 05:52:29 +0530

    sale

---
+++ .ipynb_checkpoints/P100-checkpoint.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.write("Hello World")
+
+st.title('Hello World')
+
+
+button = st.button('click me')
+
+if button:
+    st.write('Clice Button')
+
+
+
+
+
+
